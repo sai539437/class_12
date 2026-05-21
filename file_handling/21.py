b5f79ef980e8b4b4cd6fcb90d6b719e72172dfbe
sai539437 <saiaadyasha720@gmail.com>
2026-05-21 11:12:23 +0000

    update

--- file_handling/21.py
+++ file_handling/21.py
@@ -1,5 +1,4 @@
 #Write a function that receives two string arguments and checks whether they are same-length strings (returns True in this case otherwise False).
-
 s1 = input("Enter first string: ")
 s2 = input("Enter second string: ")
 
