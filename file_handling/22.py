991e9ec71b01633992815bdf526dda51142b64d6
sai539437 <saiaadyasha720@gmail.com>
2026-05-21 11:21:36 +0000

    update

--- file_handling/22.py
+++ file_handling/22.py
@@ -1,4 +1,5 @@
-#Write a function that takes two numbers and returns the number that has minimum one's digit.  
+#Write a function that takes two numbers and returns the number that has minimum one's digit. 
+#  
 #  [For example, if numbers passed are 491 and 278, then the function will return 491 because it has got minimum one's digit
 #  out of two given numbers (491's 1 is < 278's 8)].  
 def min_o_d(n1, n2):
